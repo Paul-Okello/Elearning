5235021e0fb9d13e11bcbaeb381c0491d52bdf27
Paul Ochieng <paulokello273@gmail.com>
2024-12-18 12:41:43 +0300

    Enhance Dashboard Pages with Course Preloading and User Authentication
    - Updated the StudentPage to preload courses using Convex and display them in a CourseList component.
    - Enhanced the TeacherCourses page by implementing user authentication, redirecting unauthenticated users to the sign-in page, and preloading courses specific to the authenticated user for better course management.
    - Improved the layout of the TeacherCourses page by adding a CoursesTable component to display preloaded courses.

--- src/app/(dashboard)/index/page.tsx
+++ src/app/(dashboard)/index/page.tsx
@@ -1,5 +1,13 @@
+import { api } from "@/_generated/api";
+import CourseList from "@/components/courses/course-list";
+import { preloadQuery } from "convex/nextjs";
 import React from "react";
 
 export default async function StudentPage() {
-	return <div>StudentPage</div>;
+	const preloadCourses = await preloadQuery(api.course.course.getAllCourses);
+	return (
+		<div className="p-4">
+			<CourseList preloadCourses={preloadCourses} />
+		</div>
+	);
 }
--- src/app/(dashboard)/teacher/courses/page.tsx
+++ src/app/(dashboard)/teacher/courses/page.tsx
@@ -1,10 +1,24 @@
+import { api } from "@/_generated/api";
+import CoursesTable from "@/app/(dashboard)/teacher/courses/_components/courses-table";
 import CreateCourseCard from "@/components/courses/create-course-card";
+import { auth } from "@clerk/nextjs/server";
+import { preloadQuery } from "convex/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function TeacherCourses() {
+	const { userId } = await auth();
+	if (!userId) redirect("/sign-in");
+	const preloadedCourses = await preloadQuery(
+		api.course.course.getCoursesByUser,
+		{
+			clerkId: userId,
+		},
+	);
 	return (
-		<div className="p-6">
+		<div className="p-6 space-y-4">
 			<CreateCourseCard />
+			<CoursesTable preloadedCourses={preloadedCourses} />
 		</div>
 	);
 }
