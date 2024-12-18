80a88a5cd0c3d0eca4c9868bb5edec82e4b32d30
Paul Ochieng <paulokello273@gmail.com>
2024-12-18 06:58:25 +0300

    Add new utility hooks and data structures for course and file management
    - Introduced `useConfettiStore` hook for managing confetti state in the application.
    - Added `course-utils.ts` file containing predefined course categories and names for better organization and retrieval.
    - Created `uploadthing.ts` file to facilitate file uploads with helper functions from the Uploadthing library.
    
    These changes enhance the application's functionality by providing essential hooks and data structures for course management and file uploads.

---
+++ src/lib/course-utils.ts
@@ -0,0 +1,69 @@
+export const courseCategoriesFrontend = [
+	{
+		name: "Science & Technology",
+		description: "Explore science, tech, engineering, and natural sciences.",
+	},
+	{
+		name: "Programming",
+		description:
+			"Learn coding, software development, and programming languages.",
+	},
+	{
+		name: "Health & Medicine",
+		description: "Courses on healthcare, medical sciences, and wellness.",
+	},
+	{
+		name: "Business & Economics",
+		description: "Understand business principles, economics, and marketing.",
+	},
+	{
+		name: "Arts & Design",
+		description:
+			"Creative fields like art, music, film, literature, and design.",
+	},
+	{
+		name: "History & Politics",
+		description:
+			"Dive into historical events, governance, and political systems.",
+	},
+	{
+		name: "Lifestyle & Home",
+		description: "Topics on personal growth, home life, and relationships.",
+	},
+	{
+		name: "Sports & Recreation",
+		description: "Courses on sports, fitness, travel, and outdoor activities.",
+	},
+	{
+		name: "Food & Beauty",
+		description: "Learn culinary skills, beauty techniques, and wellness.",
+	},
+	{
+		name: "Spirituality & Self-help",
+		description:
+			"Courses on personal development, spirituality, and wellbeing.",
+	},
+	{
+		name: "Education",
+		description: "General learning and educational development topics.",
+	},
+	{
+		name: "Agriculture",
+		description: "Explore farming, agribusiness, and food production systems.",
+	},
+] as const;
+
+export const categoryNames = [
+	"Science & Technology",
+	"Programming",
+	"Health & Medicine",
+	"Business & Economics",
+	"Arts & Design",
+	"History & Politics",
+	"Lifestyle & Home",
+	"Sports & Recreation",
+	"Food & Beauty",
+	"Spirituality & Self-help",
+	"Education",
+	"Agriculture",
+] as const;
