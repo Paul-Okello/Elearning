b1d8c7331dcb2f426c79c40b7c2768d2ad8885ca
Paul Ochieng <paulokello273@gmail.com>
2024-12-18 07:06:30 +0300

    Add Course Editing Components</message>
    <message>
    - Introduced a new `index.ts` file to aggregate and export various course editing components, including `CourseChaptersForm`, `CourseActions`, `CourseAttachmentForm`, `CourseCategoryForm`, `CourseDescriptionForm`, `CourseImageForm`, `CoursePriceForm`, and `CourseTitleForm`.
    - This change enhances the modularity and organization of course editing functionalities, making it easier to manage and utilize these components within the application.

---
+++ src/components/courses/edit-course/index.ts
@@ -0,0 +1,20 @@
+import CourseChaptersForm from "@/components/courses/edit-course/add-chapter-form";
+import CourseActions from "@/components/courses/edit-course/course-actions";
+import CourseAttachmentForm from "@/components/courses/edit-course/course-attachment-form";
+import CourseCategoryForm from "@/components/courses/edit-course/course-category-form";
+import CourseDescriptionForm from "@/components/courses/edit-course/course-description-form";
+import CourseImageForm from "@/components/courses/edit-course/course-image-form";
+import CoursePriceForm from "@/components/courses/edit-course/course-price-form";
+import CourseTitleForm from "@/components/courses/edit-course/course-title-form";
+
+export {
+	CourseAttachmentForm,
+	CourseCategoryForm,
+	CourseChaptersForm,
+	CourseDescriptionForm,
+	CourseImageForm,
+	CoursePriceForm,
+	CourseTitleForm,
+	CourseActions,
+};
+
